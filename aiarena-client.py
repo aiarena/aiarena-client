91a54c44083dc46a238fac439125e2c69f70098a
BurnySc2 <gamingburny@gmail.com>
2019-05-21 15:50:07 +0200

    Clean up code

--- aiarena-client.py
+++ aiarena-client.py
@@ -13,174 +13,175 @@ import shutil
 
 global count
 count = 0
+
+this_folder = os.path.dirname(__file__)
 temppath = "/tmp/aiarena/"
-if not os.path.isdir(temppath):
-    os.mkdir(temppath)
+# Make temporary subfolder
+os.makedirs(temppath, exist_ok=True)
 
 os.chdir("/home/aiarena/aiarena-client")
 
-# read config file
-with open('/home/aiarena/aiarena-client/aiarena-client.json') as config_file:
+# Read config file
+with open("/home/aiarena/aiarena-client/aiarena-client.json") as config_file:
     config = json.load(config_file)
 
-# print to console and log
+# Print to console and log
 def printout(text):
     now = datetime.datetime.now()
-    print(colored(now.strftime("%b %d %H:%M:%S"), 'yellow') + " " + colored(config['id'], 'red') + " " + colored(text, 'green'))
-    f = open("/home/aiarena/aiarena-client/aiarena-client.log","a+")
-    f.write(now.strftime("%b %d %H:%M:%S") + " " + config['id'] + " " + text + "\n")
-    f.close()
+    infos = [now.strftime("%b %d %H:%M:%S"), config["id"], text]
+    # Maps yellow to the first info, red to the second, green for the text
+    colors = ["yellow", "red", "green"]
+    colored_infos = " ".join(colored(info, color) for info, color in zip(infos, colors))
+    print(colored_infos)
+    with open("/home/aiarena/aiarena-client/aiarena-client.log", "a+") as f:
+        line = " ".join(infos) + "\n"
+        f.write(line)
 
 
-# needed for hashlib md5 function
+# Needed for hashlib md5 function
 def file_as_bytes(file):
     with file:
         return file.read()
 
+
 # Get bot file from api by bot id
 def getbotfile(bot):
-    botname = bot['name']
-    boturl = bot['bot_zip']
-    botmd5 = bot['bot_zip_md5hash']
-    printout("Downloading bot " + botname)
-    botdataurl = bot['bot_data']
-    botdatamd5 = bot['bot_data_md5hash']
-    r = requests.get(boturl, headers={'Authorization': "Token " + config['token']})
-    with open(temppath + botname + ".zip", 'wb') as f:
+    botname = bot["name"]
+    boturl = bot["bot_zip"]
+    botmd5 = bot["bot_zip_md5hash"]
+    printout(f"Downloading bot {botname}")
+    # Download bot and save to .zip
+    r = requests.get(boturl, headers={"Authorization": "Token " + config["token"]})
+    bot_download_path = os.path.join(temppath, botname + ".zip")
+    with open(bot_download_path, "wb") as f:
         f.write(r.content)
-    if botmd5 == hashlib.md5(file_as_bytes(open(temppath + botname + ".zip", 'rb'))).hexdigest():
+    # Load bot from .zip to calculate md5
+    with open(bot_download_path) as f:
+        calculated_md5 = hashlib.md5(file_as_bytes(f)).hexdigest()
+    if botmd5 == calculated_md5:
         printout("MD5 hash matches transferred file...")
-        printout("Extracting bot " + botname + " to bots/" + botname)
-        zip_ref = zipfile.ZipFile(temppath + botname + ".zip", 'r')
-        zip_ref.extractall("bots/" + botname)
-        zip_ref.close()
+        printout(f"Extracting bot {botname} to bots/{botname}")
+        # Extract to bot folder
+        with zipfile.ZipFile(bot_download_path, "r") as zip_ref:
+            zip_ref.extractall(f"bots/{botname}")
         if getbotdatafile(bot):
-            return 1
+            return True
+        else:
+            return False
     else:
-        printout("MD5 hash (" + botmd5 + ") doesent match transferred file (" + hashlib.md5(file_as_bytes(open(temppath + botname + ".zip", 'rb'))).hexdigest() + ")")
+        printout(f"MD5 hash ({botmd5}) does not match transferred file ({calculated_md5})")
         cleanup()
-        return 0
+        return False
 
 
 # Get bot data
 def getbotdatafile(bot):
-    botname = bot['name']
-    if bot['bot_data'] == None:
-        return 1
+    botname = bot["name"]
+    if bot["bot_data"] is None:
+        return True
 
-    botdataurl = bot['bot_data']
-    botdatamd5 = bot['bot_data_md5hash']
-    printout("Downloading bot data for " + botname)
-    r = requests.get(botdataurl, headers={'Authorization': "Token " + config['token']})
-    with open(temppath + botname + "-data.zip", 'wb') as f:
+    botdataurl = bot["bot_data"]
+    botdatamd5 = bot["bot_data_md5hash"]
+    printout(f"Downloading bot data for {botname}")
+    # Download bot data and save to .zip
+    r = requests.get(botdataurl, headers={"Authorization": "Token " + config["token"]})
+    bot_data_path = os.path.join(temppath, botname + "-data.zip")
+    with open(bot_data_path, "wb") as f:
         f.write(r.content)
-    if botdatamd5 == hashlib.md5(file_as_bytes(open(temppath + botname + "-data.zip", 'rb'))).hexdigest():
+    with open(bot_data_path) as f:
+        calculated_md5 = hashlib.md5(file_as_bytes(f)).hexdigest()
+    if botdatamd5 == calculated_md5:
         printout("MD5 hash matches transferred file...")
-        printout("Extracting data for " + botname + " to bots/" + botname + "/data")
-        zip_ref = zipfile.ZipFile(temppath + botname + "-data.zip", 'r')
-        zip_ref.extractall("bots/" + botname + "/data")
-        zip_ref.close()
-        return 1
+        printout(f"Extracting data for {botname} to bots/{botname}/data")
+        with zipfile.ZipFile(bot_data_path, "r") as zip_ref:
+            zip_ref.extractall(f"bots/{botname}/data")
+        return True
     else:
-        printout("MD5 hash (" + botdatamd5 + ") doesent match transferred file (" + hashlib.md5(file_as_bytes(open(temppath + botname + "-data.zip", 'rb'))).hexdigest() + ")")
+        printout(f"MD5 hash ({botdatamd5}) does not match transferred file ({calculated_md5})")
         cleanup()
-        return 0
+        return False
 
 
 # Get bot file
 def getbotdata(bot):
-    botname = bot['name']
-    botrace = bot['plays_race']
-    bottype = bot['type']
+    botname = bot["name"]
+    botrace = bot["plays_race"]
+    bottype = bot["type"]
 
-    bot_data = {}
+    race_map = {"P": "Protoss", "T": "Terran", "Z": "Zerg", "R": "Random"}
+    bot_type_map = {
+        "python": ["run.py", "Python"],
+        "cppwin32": [f"{botname}.exe", "Wine"],
+        "cpplinux": [f"{botname}", "BinaryCpp"],
+        "dotnetcore": [f"{botname}.dll", "DotNetCore"],
+        "java": [f"{botname}.jar", "Java"],
+        "nodejs": ["main.jar", "NodeJS"],
+    }
 
-    if botrace == 'P':
-        bot_data['Race'] = 'Protoss'
-    elif botrace == 'T':
-        bot_data['Race'] = 'Terran'
-    elif botrace == 'Z':
-        bot_data['Race'] = 'Zerg'
-    elif botrace == 'R':
-        bot_data['Race'] = 'Random'
-
-    bot_data['RootPath'] = "/home/aiarena/aiarena-client/bots/" + botname
-
-    if bottype == 'python':
-        bot_data['FileName'] = 'run.py'
-        bot_data['Type'] = 'Python'
-    elif bottype == 'cppwin32':
-        bot_data['FileName'] = botname + ".exe"
-        bot_data['Type'] = 'Wine'
-    elif bottype == 'cpplinux':
-        bot_data['FileName'] = botname
-        bot_data['Type'] = 'BinaryCpp'
-    elif bottype == 'dotnetcore':
-        bot_data['FileName'] = botname + ".dll"
-        bot_data['Type'] = 'DotNetCore'
-    elif bottype == 'java':
-        bot_data['FileName'] = botname + ".jar"
-        bot_data['Type'] = 'Java'
-    elif bottype == 'nodejs':
-        bot_data['FileName'] = "main.js"
-        bot_data['Type'] = 'NodeJS'
-
-    return(botname, bot_data)
+    bot_data = {
+        "Race": race_map[botrace],
+        "RootPath": f"/home/aiarena/aiarena-client/bots/{botname}",
+        "FileName": bot_type_map[bottype][0],
+        "Type": bot_type_map[bottype][1],
+    }
+    return botname, bot_data
 
 
 def getnextmatch():
     global count
-    nextmatchresponse = requests.post('https://ai-arena.net/api/arenaclient/matches/', headers={'Authorization': "Token " + config['token']})
+    nextmatchresponse = requests.post(
+        "https://ai-arena.net/api/arenaclient/matches/", headers={"Authorization": "Token " + config["token"]}
+    )
     nextmatchdata = json.loads(nextmatchresponse.text)
 
-    if not "id" in nextmatchdata:
-        printout('No games available - sleeping')
+    if "id" not in nextmatchdata:
+        printout("No games available - sleeping")
         time.sleep(30)
         cleanup()
-        count = count - 1
+        count -= 1
         return
 
-    nextmatchid = nextmatchdata['id']
-    printout("Next match: " + str(nextmatchid))
+    nextmatchid = nextmatchdata["id"]
+    printout(f"Next match: {nextmatchid}")
 
-    mapname = nextmatchdata['map']['name']
-    mapurl = nextmatchdata['map']['file']
+    mapname = nextmatchdata["map"]["name"]
+    mapurl = nextmatchdata["map"]["file"]
     r = requests.get(mapurl)
-    printout("Downloading map " + mapname)
-    with open("/home/aiarena/StarCraftII/maps/" + mapname + ".SC2Map", 'wb') as f:
+    printout(f"Downloading map {mapname}")
+    map_path = f"/home/aiarena/StarCraftII/maps/{mapname}.SC2Map"
+    with open(map_path, "wb") as f:
         f.write(r.content)
 
-    bot_0 = nextmatchdata['bot1']
+    bot_0 = nextmatchdata["bot1"]
     if not getbotfile(bot_0):
+        count -= 1
         cleanup()
-        count = count - 1
         return
-    bot_1 = nextmatchdata['bot2']
+    bot_1 = nextmatchdata["bot2"]
     if not getbotfile(bot_1):
+        count -= 1
         cleanup()
-        count = count - 1
         return
 
-    (bot_0_name, bot_0_data) = getbotdata(bot_0)
-    (bot_1_name, bot_1_data) = getbotdata(bot_1)
+    bot_0_name, bot_0_data = getbotdata(bot_0)
+    bot_1_name, bot_1_data = getbotdata(bot_1)
 
-    # Write matchuplist file
-    with open('matchuplist', 'w') as f:
-        f.write("\"" + str(bot_0['name']) + "\"vs\"" + str(bot_1['name']) + "\"" + str(mapname) + ".SC2Map")
+    # Write matchuplist file, line should match something like
+    # "m1ndbot"vs"m2ndbot" AutomatonLE.SC2Map
+    match_line = f'"{bot_0_name}"vs"{bot_1_name}" {mapname}.SC2Map'
+    with open("matchuplist", "w") as f:
+        f.write(match_line)
 
     # Write LadderBots.json file
-    botconfig = {}
-    botconfig[bot_0_name] = bot_0_data
-    botconfig[bot_1_name] = bot_1_data
-
-    ladderbots = { "Bots": botconfig}
+    ladderbots = {"Bots": {bot_0_name: bot_0_data, bot_1_name: bot_1_data}}
     ladderbots_json = json.dumps(ladderbots, indent=4, sort_keys=True)
 
-    with open('LadderBots.json', 'w') as f:
+    with open("LadderBots.json", "w") as f:
         f.write(ladderbots_json)
 
     runmatch()
 
+    # Wait for result.json
     while not os.path.exists("/home/aiarena/aiarena-client/results.json"):
         time.sleep(1)
 
@@ -188,75 +189,99 @@ def getnextmatch():
         printout("Game finished")
         postresult(nextmatchdata)
 
+
 def runmatch():
-    printout("Starting Game - Round " + str(count))
-    subprocess.Popen(["/home/aiarena/aiarena-client/Sc2LadderServer","-e","/home/aiarena/StarCraftII/Versions/Base70154/SC2_x64"], stdout=DEVNULL, stderr=DEVNULL)
+    printout(f"Starting Game - Round {count}")
+    subprocess.Popen(
+        ["/home/aiarena/aiarena-client/Sc2LadderServer", "-e", "/home/aiarena/StarCraftII/Versions/Base70154/SC2_x64"],
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+    )
+
 
 def postresult(match):
     global count
     # Parse results.json
-    with open('/home/aiarena/aiarena-client/results.json') as results_json_file:
+    with open("/home/aiarena/aiarena-client/results.json") as results_json_file:
         resultdata = json.load(results_json_file)
-        for p in resultdata['Results']:
-            result = p['Result']
-            gametime = p['GameTime']
+    for p in resultdata["Results"]:
+        result = p["Result"]
+        gametime = p["GameTime"]
+
+    replay_folder = "/home/aiarena/aiarena-client/replays"
 
     # Collect the replayfile
-    replayfile = ''
-    for file in os.listdir("/home/aiarena/aiarena-client/replays"):
+    replayfile = ""
+    for file in os.listdir(replay_folder):
         if file.endswith(".SC2Replay"):
             replayfile = file
+            break
+    replay_file_path = os.path.join(replay_folder, replayfile)
 
-    if not os.path.isdir("/home/aiarena/aiarena-client/bots/" + match['bot1']['name'] + "/data"):
-        os.mkdir("/home/aiarena/aiarena-client/bots/" + match['bot1']['name'] + "/data")
-    shutil.make_archive(temppath + match['bot1']['name'] + "-data", 'zip', "/home/aiarena/aiarena-client/bots/" + match['bot1']['name'] + "/data")
-    if not os.path.isdir("/home/aiarena/aiarena-client/bots/" + match['bot2']['name'] + "/data"):
-        os.mkdir("/home/aiarena/aiarena-client/bots/" + match['bot2']['name'] + "/data")
-    shutil.make_archive(temppath + match['bot2']['name'] + "-data", 'zip', "/home/aiarena/aiarena-client/bots/" + match['bot2']['name'] + "/data")
+    bot_1_name = match["bot1"]["name"]
+    bot_2_name = match["bot2"]["name"]
+    bot1_data_folder = f"/home/aiarena/aiarena-client/bots/{bot_1_name}/data"
+    bot2_data_folder = f"/home/aiarena/aiarena-client/bots/{bot_2_name}/data"
 
-    if os.path.isfile("/home/aiarena/aiarena-client/replays/" + replayfile):
+    # Create downloable data archives
+    if not os.path.isdir(bot1_data_folder):
+        os.mkdir(bot1_data_folder)
+    shutil.make_archive(temppath + match["bot1"]["name"] + "-data", "zip", bot1_data_folder)
+    if not os.path.isdir(bot2_data_folder):
+        os.mkdir(bot2_data_folder)
+    shutil.make_archive(temppath + match["bot2"]["name"] + "-data", "zip", bot2_data_folder)
+
+    results_website = "https://ai-arena.net/api/arenaclient/results/"
+
+    # Upload replay file and bot data archives
+    if os.path.isfile(replay_file_path):
         file_list = {
-            'replay_file': open("/home/aiarena/aiarena-client/replays/" + replayfile, 'rb'),
-            'bot1_data': open(temppath + match['bot1']['name'] + "-data.zip", 'rb'),
-            'bot2_data': open(temppath + match['bot2']['name'] + "-data.zip", 'rb')
+            "replay_file": open(replay_file_path, "rb"),
+            "bot1_data": open(temppath + match["bot1"]["name"] + "-data.zip", "rb"),
+            "bot2_data": open(temppath + match["bot2"]["name"] + "-data.zip", "rb"),
         }
-        payload = {'type': result, 'match': int(match['id']), 'duration': gametime}
-        post = requests.post("https://ai-arena.net/api/arenaclient/results/", files=file_list, data=payload, headers={'Authorization': "Token " + config['token']})
+        payload = {"type": result, "match": int(match["id"]), "duration": gametime}
+        post = requests.post(
+            results_website, files=file_list, data=payload, headers={"Authorization": "Token " + config["token"]}
+        )
         printout(result + " - Result transferred")
     else:
-        payload = {'type': result, 'match': int(match['id']), 'duration': gametime}
-        post = requests.post("https://ai-arena.net/api/arenaclient/results/", data=payload, headers={'Authorization': "Token " + config['token']})
+        payload = {"type": result, "match": int(match["id"]), "duration": gametime}
+        post = requests.post(results_website, data=payload, headers={"Authorization": "Token " + config["token"]})
         printout(result + " - Result transferred")
 
 
 def cleanup():
-    if os.path.isfile("/home/aiarena/aiarena-client/matchuplist"):
-        os.remove("/home/aiarena/aiarena-client/matchuplist")
-    if os.path.isfile("/home/aiarena/aiarena-client/LadderBots.json"):
-        os.remove("/home/aiarena/aiarena-client/LadderBots.json")
-    if os.path.isfile("/home/aiarena/aiarena-client/playerids"):
-        os.remove("/home/aiarena/aiarena-client/playerids")
-    if os.path.isfile("/home/aiarena/aiarena-client/results.json"):
-        os.remove("/home/aiarena/aiarena-client/results.json")
+    # Files to remove
+    files = [
+        "/home/aiarena/aiarena-client/matchuplist",
+        "/home/aiarena/aiarena-client/LadderBots.json",
+        "/home/aiarena/aiarena-client/playerids",
+        "/home/aiarena/aiarena-client/results.json",
+    ]
 
-    for file in os.listdir("/home/aiarena/aiarena-client/replays"):
-        os.remove("/home/aiarena/aiarena-client/replays/" + file)
+    for file in files:
+        if os.path.isfile(file):
+            os.remove(file)
 
-    for file in os.listdir(temppath):
-        os.remove(temppath + file)
+    # Files to remove inside these folders
+    folders = ["/home/aiarena/aiarena-client/replays", temppath]
+    for folder in folders:
+        for file in os.listdir(folder):
+            file_path = os.path.join(folder, file)
+            os.remove(file_path)
 
+    # Remove entire subfolders
     for dir in os.listdir("/home/aiarena/aiarena-client/bots"):
         shutil.rmtree("/home/aiarena/aiarena-client/bots/" + dir)
 
 
-while count <= config['rounds']:
-    count = count + 1
+while count <= config["rounds"]:
+    count += 1
     cleanup()
     getnextmatch()
 
-if config['shutdown'] == "true":
+if config["shutdown"] == "true":
     printout("Stopping system")
-    f = open("/home/aiarena/aiarena-client/.shutdown","w")
-    f.write("Shutdown")
-    f.close()
-    
+    with open("/home/aiarena/aiarena-client/.shutdown", "w") as f:
+        f.write("Shutdown")
