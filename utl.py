ebf1a2531e7492b318562591a41b5dfec9068d0d
lladdy <randallboland@bigpond.com>
2019-08-04 03:32:40 +0930

    Cleanup process detection code

--- utl.py
+++ utl.py
@@ -1,6 +1,6 @@
 import datetime
-import os
 
+import psutil
 from termcolor import colored
 
 import config
@@ -46,47 +46,5 @@ def load_pid_from_file(pid_file):
         return None
 
 
-# https://www.madebuild.org/blog/?p=30
-import os
-import platform
-
-# GetExitCodeProcess uses a special exit code to indicate that the process is
-# still running.
-_STILL_ACTIVE = 259
-
-import psutil
-
 def is_pid_running(pid):
     return pid in (p.pid for p in psutil.process_iter())
-
-# def is_pid_running(pid):
-#     return (_is_pid_running_on_windows(pid) if platform.system() == "Windows"
-#             else _is_pid_running_on_unix(pid))
-
-
-def _is_pid_running_on_unix(pid):
-    try:
-        os.kill(pid, 0)
-    except OSError:
-        return False
-    return True
-
-
-def _is_pid_running_on_windows(pid):
-    import ctypes.wintypes
-
-    kernel32 = ctypes.windll.kernel32
-    handle = kernel32.OpenProcess(1, 0, pid)
-    if handle == 0:
-        return False
-
-    # If the process exited recently, a pid may still exist for the handle.
-    # So, check if we can get the exit code.
-    exit_code = ctypes.wintypes.DWORD()
-    is_running = (
-            kernel32.GetExitCodeProcess(handle, ctypes.byref(exit_code)) == 0)
-    kernel32.CloseHandle(handle)
-
-    # See if we couldn't get the exit code or the exit code indicates that the
-    # process is still running.
-    return is_running or exit_code.value == _STILL_ACTIVE
