8df25120dd464dbbc9245db902173a65bcd88796
lladdy <gitlab@randallboland.com>
2019-10-05 00:45:40 +0000

    Lladdy 1 cleanup config

--- default_config.py
+++ default_config.py
@@ -16,14 +16,13 @@ ARENA_CLIENT_ID = "aiarenaclient_000"
 API_TOKEN = "12345"
 ROUNDS_PER_RUN = 5
 SHUT_DOWN_AFTER_RUN = True
-RUN_REPLAY_CHECK = True
 USE_PID_CHECK = False
 DEBUG_MODE = True
 PYTHON = "python3"
 RUN_LOCAL = False
+CLEANUP_BETWEEN_ROUNDS = True
 SYSTEM = platform.system()
 SC2_PROXY = {"HOST": "127.0.0.1", "PORT": 8765}
-
 
 # LOGGING
 LOGGING_HANDLER = logging.FileHandler("supervisor.log", "a+")
@@ -34,7 +33,9 @@ TEMP_PATH = "/tmp/aiarena/"
 LOCAL_PATH = os.path.dirname(__file__)
 WORKING_DIRECTORY = LOCAL_PATH  # same for now
 LOG_FILE = os.path.join(WORKING_DIRECTORY, "aiarena-client.log")
+RESULTS_LOG_FILE = os.path.join(WORKING_DIRECTORY, "results.log")
 REPLAYS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "replays")
+BOTS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "bots")
 
 # WEBSITE
 BASE_WEBSITE_URL = "https://ai-arena.net"
@@ -44,36 +45,6 @@ API_RESULTS_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/results/")
 # STARCRAFT
 SC2_HOME = "/home/aiarena/StarCraftII/"
 SC2_BINARY = os.path.join(SC2_HOME, "Versions/Base75689/SC2_x64")
-
-# SC2LADDERSERVER
-SC2LADDERSERVER_PID_FILE = os.path.join(LOCAL_PATH, "laddermanager.pid")
-SC2LADDERSERVER_PID_FILE_CREATION_TIMEOUT = 10
-SC2LADDERSERVER_STDOUT_FILE = os.path.join(LOCAL_PATH, "sc2ladderserver_stdout.log")
-SC2LADDERSERVER_STDERR_FILE = os.path.join(LOCAL_PATH, "sc2ladderserver_stderr.log")
-SC2LADDERSERVER_BINARY = os.path.join(LOCAL_PATH, "Sc2LadderServer")
-SC2LADDERSERVER_MATCHUP_LIST_FILE = os.path.join(LOCAL_PATH, "matchuplist")
-SC2LADDERSERVER_LADDERBOTS_FILE = os.path.join(LOCAL_PATH, "LadderBots.json")
-SC2LADDERSERVER_PLAYERIDS_FILE = os.path.join(LOCAL_PATH, "playerids")
-SC2LADDERSERVER_RESULTS_FILE = os.path.join(LOCAL_PATH, "results.json")
-SC2LADDERSERVER_CONFIG_FILE = os.path.join(LOCAL_PATH, "LadderManager.json")
-# todo: download relevant settings from the API
-SC2LADDERSERVER_CONFIG_JSON = (
-    "{"
-    '    "LocalReplayDirectory": "./replays/",'
-    '    "MaxGameTime": 60480,'
-    '    "MatchupGenerator": "File",'
-    '    "MatchupListFile": "./matchuplist",'
-    '    "ErrorListFile": "./errorlist",'
-    '    "BotConfigFile": "./LadderBots.json",'
-    '    "EnableReplayUpload": "False",'
-    '    "ResultsLogFile": "./results.json",'
-    '    "PlayerIdFile": "./playerids",'
-    '    "PythonBinary": "/home/aiarena/venv/bin/python",'
-    '    "RealTimeMode": false,'
-    '    "Maps": [],'
-    '    "MaxFrameTime": 20000'
-    "}"
-)
 MAX_GAME_TIME = 60486
 MAX_FRAME_TIME = 1000
 STRIKES = 10
