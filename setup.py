ddbbfca5c26c72f39e6531bfcfea9d4e54bee32e
danielvschoor <43602193+danielvschoor@users.noreply.github.com>
2019-11-12 17:37:27 +0200

    Remove Flask server

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arenaclient',
+    version='0.1',
+    packages=['arenaclient','arenaclient/proxy','arenaclient/sc2','arenaclient/sc2/dicts','arenaclient/sc2/helpers','arenaclient/sc2/ids','arenaclient/imagezmq']
+)
