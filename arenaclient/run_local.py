2758c14c119e45114b121be5ed76a37564b6fbaa
BuRny <gamingburny@gmail.com>
2019-12-07 11:31:28 +0100

    Update run_local.py file

--- arenaclient/run_local.py
+++ arenaclient/run_local.py
@@ -1,21 +1,46 @@
-import os
+import asyncio
+from multiprocessing import Process
+from typing import List
 
-import arenaclient.default_local_config as config
-
-from arenaclient.client import Client
-from arenaclient.utl import Utl
-
-# Sanity check the config and remind people to check their config
-utl = Utl(config)
+from arenaclient.proxy.frontend import GameRunner
+from arenaclient.proxy.server import run_server
 
 
-games = ['loser_bot,T,python,loser_bot,T,python,AutomatonLE']
-config.ROUNDS_PER_RUN = 1
-games_len = len(games)
-config.REALTIME = True
-for key in games:    
-    with open(config.MATCH_SOURCE_CONFIG.MATCHES_FILE, "w+") as f:
-        f.write(key + os.linesep)
+def generate_games_list(bot1_list: List[str], bot2_list: List[str], map_list: List[str]) -> List[str]:
+    """
+    Generates games list, every bot from 'bot1_list' will be matched against every bot from 'bot2_list' on every map in 'map_list'.
 
-    ac = Client(config)
-    ac.run()
+    Example input:
+        generate_games_list(["CreepyBot,Z,python"], ["basic_bot,T,python", "loser_bot,T,python], ["AcropolisLE", "TritonLE"])
+    """
+    games = []
+    for bot1_string in bot1_list:
+        for bot2_string in bot2_list:
+            for map_name in map_list:
+                games.append(",".join([bot1_string, bot2_string, map_name]))
+    return games
+
+
+async def main():
+    # Start proxy server
+    proc = Process(target=run_server, args=[False])
+    proc.daemon = True
+    proc.start()
+
+    runner = GameRunner()
+    bot1_list = ["basic_bot,T,python"]
+    bot2_list = ["loser_bot,T,python"]
+    map_list = ["AcropolisLE"]
+    games = generate_games_list(bot1_list, bot2_list, map_list)
+    # games = ["basic_bot,T,python,loser_bot,T,python,AcropolisLE"]
+
+    data = {"Realtime": False, "Visualize": False}
+    await runner.run_local_game(games, data)
+
+    # Games have run, but server does not shut down automatically, kill server process
+    proc.terminate()
+
+
+if __name__ == "__main__":
+    # Python 3.7+
+    asyncio.run(main())
