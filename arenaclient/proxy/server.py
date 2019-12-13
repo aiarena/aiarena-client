b1d6970cc2a47ff308161c659a8c4dd2f481c492
danielvschoor <43602193+danielvschoor@users.noreply.github.com>
2019-12-13 18:01:17 +0200

    Change log url

--- arenaclient/proxy/server.py
+++ arenaclient/proxy/server.py
@@ -255,7 +255,7 @@ def run_server(use_frontend=None):
             web.get("/get_arena_bots", frontend.get_arena_bots, name='get_arena_bots'),
             web.get("/get_maps", frontend.get_maps, name='get_maps'),
             web.get("/replays/{replay}", frontend.replays, name='replays'),
-            web.get("/logs/{match_id}/{bot_name}", frontend.logs, name='logs')
+            web.get("/logs/{match_id}/{bot_name}/stderr.log", frontend.logs, name='logs')
         ]
         app.router.add_routes(routes)
     on_start()
