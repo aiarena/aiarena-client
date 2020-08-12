1382a3fb09da0c64aacd9e3603db74d0ed5ce604
danielvschoor <danielvanschoor1997@gmail.com>
2020-08-12 13:01:35 +0200

    Bump to 0.1.12

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=[
-        "rust_arenaclient==0.1.11",
+        "rust_arenaclient==0.1.12",
         "requests==2.24.0",
         "aiohttp==3.6.2",
         "termcolor==1.1.0",
