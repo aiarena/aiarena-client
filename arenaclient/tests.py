94c6974aeffd01175f22318650e651598ac6070d
danielvschoor <danielvanschoor1997@gmail.com>
2021-06-12 16:27:57 +0200

    Integrate tests into __main__.py

---
+++ arenaclient/tests.py
@@ -0,0 +1,99 @@
+import json
+import os
+import asyncio
+import shutil
+import logging
+from arenaclient.configs import default_test_config as config
+
+from arenaclient.client import Client
+from arenaclient.match.matches import MatchSourceType
+from arenaclient.utl import Utl
+from pathlib import Path
+
+logging.getLogger().setLevel(logging.DEBUG)  # Logging needs to be initialized before importing rust_ac
+logging.info("")
+from rust_ac import Server
+
+
+def test_assertions():
+    assert config.TEST_MODE, "LOCAL_TEST_MODE config value must must be set to True to run tests." + os.linesep \
+                             + "IMPORTANT: Are you configured properly for tests?"
+    assert config.MATCH_SOURCE_CONFIG.TYPE == MatchSourceType.FILE, "MatchSourceType must be set to FILE to run " \
+                                                                    "tests." \
+                                                                    + os.linesep \
+                                                                    + "IMPORTANT: Are you configured properly for " \
+                                                                      "tests? "
+
+
+class IntegrationTest:
+    def __init__(self, matches_json, iterations=1):
+        self.matches = matches_json
+        self.utl = Utl(config)
+        self.iterations = iterations
+        self.original_max_time = config.MAX_GAME_TIME
+
+    async def run_tests(self):
+        """
+        Run tests.
+        """
+        # Sanity check the config and remind people to check their config
+        test_assertions()
+        with open('test_results.txt', 'w+') as f:  # Clear results file
+            f.write('')
+
+        for it in range(self.iterations):
+            for key, value in self.matches.items():
+
+                with open(config.MATCH_SOURCE_CONFIG.MATCHES_FILE, "w+") as f:
+                    f.write(key + os.linesep)
+                if key == 'loser_bot,T,python,loser_bot,T,python,AutomatonLE':
+                    config.MAX_GAME_TIME = 1000
+                else:
+                    config.MAX_GAME_TIME = self.original_max_time
+
+                ac = Client(config)
+                await ac.run()
+
+                try:
+                    with open(config.MATCH_SOURCE_CONFIG.RESULTS_FILE, "r") as f:
+                        result = json.load(f)
+                    test_result = f"Result ({str(result['Results'][0]['Result'])}) " \
+                                  f"matches expected result ({value}):" + \
+                                  str(result["Results"][0]["Result"] == value)
+                    self.utl.printout(test_result)
+                    assert (str(result['Results'][0]['Result']) == value)
+                    with open('test_results.txt', 'a+') as f:
+                        f.write(str(key) + '\t' + str(test_result) + '\n')
+                except FileNotFoundError:
+                    self.utl.printout("Test failed: Results file not found")
+                except KeyError:
+                    self.utl.printout("Test failed: Result not found in file")
+
+
+def setup_bots():
+    bots_path = Path("../aiarena-test-bots")
+    to_bots_path = Path("bots/")
+
+    if not bots_path.exists():
+        raise NotADirectoryError(f"{bots_path} does not exist")
+    elif len([x for x in bots_path.iterdir() if x.is_dir()]) == 0:
+        raise FileNotFoundError(f"{bots_path} is empty. Did you do git clone --recursive?")
+    else:
+        if not to_bots_path.exists():
+            shutil.copytree(bots_path, to_bots_path)
+
+
+def cleanup():
+    bots_path = Path("bots/")
+    if bots_path.exists():
+        bots_path.rmdir()
+
+
+if __name__ == "__main__":
+    s = Server('127.0.0.1:8642')
+    s.run()
+    # setup_bots()
+
+    asyncio.get_event_loop().run_until_complete(run_tests())
+    s.kill()
+    # cleanup()
---
+++ test_config.py
@@ -0,0 +1,80 @@
+##########################################################
+#                                                        #
+# DEFAULT TEST CONFIG                                    #
+#                                                        #
+# !!!! DO NOT UPDATE THIS FILE WITH LOCAL SETTINGS !!!!  #
+# Create a test_config.py file to override config values #
+#                                                        #
+##########################################################
+import logging
+import os
+import platform
+from urllib import parse
+
+from arenaclient.match.matches import FileMatchSource
+
+# GERERAL
+ARENA_CLIENT_ID = "aiarenaclient_test"
+API_TOKEN = ""
+ROUNDS_PER_RUN = 1
+SHUT_DOWN_AFTER_RUN = True
+USE_PID_CHECK = False
+DEBUG_MODE = True
+PYTHON = "python"
+TEST_MODE = True
+CLEANUP_BETWEEN_ROUNDS = False
+SYSTEM = platform.system()
+SC2_PROXY = {"HOST": "127.0.0.1", "PORT": 8642}
+RUN_LOCAL = True
+
+# Secure mode will ignore the BOTS_DIRECTORY config setting and instead run each bot in their home directory.
+SECURE_MODE = False
+# Specify the users (if any) to run the bots as.
+RUN_PLAYER1_AS_USER = None
+RUN_PLAYER2_AS_USER = None
+
+
+# LOGGING
+LOGGING_HANDLER = logging.FileHandler("supervisor.log", "a+")
+LOGGING_LEVEL = 10
+
+# PATHS AND FILES
+TEMP_PATH = "/tmp/aiarena/"
+LOCAL_PATH = os.path.dirname(__file__)
+WORKING_DIRECTORY = LOCAL_PATH  # same for now
+LOG_FILE = os.path.join(WORKING_DIRECTORY, "client.log")
+REPLAYS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "replays")
+BOTS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "aiarena-test-bots")
+BOT_LOGS_DIRECTORY = os.path.join(WORKING_DIRECTORY, "logs")
+
+MATCH_SOURCE_CONFIG = FileMatchSource.FileMatchSourceConfig(
+    matches_file=os.path.join(WORKING_DIRECTORY, "matches"),
+    results_file=os.path.join(WORKING_DIRECTORY, "results")
+)
+
+# WEBSITE
+BASE_WEBSITE_URL = "https://ai-arena.net"
+API_MATCHES_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/matches/")
+API_RESULTS_URL = parse.urljoin(BASE_WEBSITE_URL, "/api/arenaclient/results/")
+
+# STARCRAFT
+MAX_GAME_TIME = 60486
+MAX_REAL_TIME = 7200  # 2 hours in seconds
+MAX_FRAME_TIME = 1000
+STRIKES = 10
+REALTIME = False
+VISUALIZE = False
+
+# MATCHES
+DISABLE_DEBUG = True
+VALIDATE_RACE = False
+
+
+# Override values with environment specific config
+try:
+    from test_config import *
+except ImportError as e:
+    if e.name == "test_config":
+        pass
+    else:
+        raise
